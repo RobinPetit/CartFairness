18eab7999ab296493bce4c65d427da19c3c09d9f
RobinPetit <robpetit@ulb.ac.be>
2025-04-10 19:04:02 +0200

    Big x3 speedup

--- CART.pyx
+++ CART.pyx
@@ -237,6 +237,10 @@ cdef class Dataset:
     cdef size_t _size
     cdef list _reverse_mapping
 
+    cdef np.float64_t[:, :] _indexed_X
+    cdef np.float64_t[:] _indexed_y
+    cdef np.float64_t[:] _indexed_p
+
     def __init__(self, np.ndarray[object, ndim=2] X,
                   np.ndarray[np.float64_t, ndim=1] y,
                   np.ndarray[np.float64_t, ndim=1] p,
@@ -245,6 +249,9 @@ cdef class Dataset:
         self._p = p
         self._is_categorical = np.zeros(X.shape[1], dtype=np.uint8)
         self._X = np.empty_like(X, dtype=np.float64)
+        self._indexed_X = None
+        self._indexed_y = None
+        self._indexed_p = None
         self._size = X.shape[0]
         self._indices = np.arange(self._size, dtype=int)
         self._reverse_mapping = [[] for _ in range(X.shape[1])]
@@ -274,6 +281,9 @@ cdef class Dataset:
         ret._indices = np.asarray(self._indices)[indices]
         ret._size = ret._indices.shape[0]
         ret._reverse_mapping = self._reverse_mapping
+        ret._indexed_X = None
+        ret._indexed_y = None
+        ret._indexed_p = None
         return ret
 
     cpdef bint is_categorical(self, int feature_idx):
@@ -311,15 +321,21 @@ cdef class Dataset:
 
     @property
     def X(self):
-        return np.asarray(self._X)[np.asarray(self._indices), :]
+        if self._indexed_X is None:
+            self._indexed_X = np.asarray(self._X)[np.asarray(self._indices), :]
+        return self._indexed_X
 
     @property
     def y(self):
-        return np.asarray(self._y)[np.asarray(self._indices)]
+        if self._indexed_y is None:
+            self._indexed_y = np.asarray(self._y)[np.asarray(self._indices)]
+        return self._indexed_y
 
     @property
     def p(self):
-        return np.asarray(self._p)[np.asarray(self._indices)]
+        if self._indexed_p is None:
+            self._indexed_p = np.asarray(self._p)[np.asarray(self._indices)]
+        return self._indexed_p
 
     cdef np.ndarray transform(self, np.ndarray X):
         cdef np.ndarray ret = np.empty_like(X, dtype=np.float64)
@@ -333,14 +349,15 @@ cdef class Dataset:
 
     cdef np.ndarray order_categorical(self, int feature_idx):
         cdef int size = len(self._reverse_mapping[feature_idx])
-        cdef np.ndarray ysums = np.zeros(size, dtype=np.float64)
-        cdef np.ndarray ysizes = np.zeros(size, dtype=np.int32)
+        cdef np.float64_t[:] ysums = np.zeros(size, dtype=np.float64)
+        cdef np.int32_t[:] ysizes = np.zeros(size, dtype=np.int32)
         _extract_mean_ys(self.X[:, feature_idx], self.y[:], ysums, ysizes)
-        cdef np.ndarray mask = np.where(ysizes > 0)[0]
-        ysums[mask] /= ysizes[mask]
+        cdef int i
+        for i in range(ysums.shape[0]):
+            if ysums[i] > 0:
+                ysums[i] /= ysizes[i]
         cdef np.ndarray ret = np.argsort(ysums)
-        ret = ret[ysizes[ret] > 0]  # TODO: optimize me
-        return ret
+        return ret[np.asarray(ysizes)[ret] > 0]
 
 @cython.final
 cdef class SplitChoice:
@@ -537,7 +554,7 @@ cdef class CART:
         ret.threshold = -1
         ret.feature_idx = -1
         self.nb_nodes += 1
-        self.idx_nodes +=1
+        self.idx_nodes += 1
         ret.dloss = 0.
         if split is None or split.left_data.get_length() <= self.minobs or \
                 split.right_data.get_length() <= self.minobs or \
@@ -647,12 +664,11 @@ cdef class CART:
         cdef SplitChoice ret  = None
         cdef SplitChoice split
 
+        start = time()
         for threshold_idx in range(values.shape[0]-1):
             # extract this into a C function
             threshold = (values[threshold_idx] + values[threshold_idx+1]) / 2.
-            start = time()
             base_idx = _masks(data.X[:, feature_idx], threshold, base_idx, sorted_indices)
-            PROBE += time() - start
             left_data = data[sorted_indices[:base_idx]]
             right_data = data[sorted_indices[base_idx:]]
 
@@ -680,6 +696,7 @@ cdef class CART:
                 )
                 if split.is_better_than(ret):
                     ret = split
+        PROBE += time() - start
         return ret
 
     cdef _find_best_threshold_categorical(
@@ -704,6 +721,7 @@ cdef class CART:
         cdef SplitChoice ret  = None
         cdef SplitChoice split
         cdef int threshold_value
+        start = time()
         for threshold_idx in range(ordered.shape[0]-1):
             threshold_value = ordered[threshold_idx]
             _mask_categorical(values, threshold_value, goes_left)
@@ -721,7 +739,7 @@ cdef class CART:
                 prop_right_p0 = np.mean(np.asarray(right_data.p) == 0)
 
                 loss = loss_left + loss_right
-                dloss = current_loss - loss #/ data.get_length()
+                dloss = current_loss - loss
 
                 if fabs(prop_left_p0 - prop_p0) > self.epsilon*prop_p0 or \
                         fabs(prop_right_p0 - prop_p0) > self.epsilon*prop_p0:
@@ -735,6 +753,7 @@ cdef class CART:
                 )
                 if split.is_better_than(ret):
                     ret = split
+        PROBE += time() - start
         return ret
 
 
